d3f86a3d1df2734e77268715bb078a735a8fe073
shariarhossain23e <shariarhossain23@gamil.comm>
2022-03-30 04:12:06 +0600

    card

--- src/component/Coin/Coin.js
+++ src/component/Coin/Coin.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Coin = ({coin}) => {
     console.log(coin);
     const {image,name,symbol} = coin
     return (
-        <div>
-            <div>
-                <img src={image} alt="" />
+        <div className='rounded-2xl shadow-lg w-[200px]'>
+           <Link to='/home'>
+           <div className='flex justify-between items-center p-4'>
+           <div>
+                <img className='w-16 h-16 object-cover mx-auto' src={image} alt="" />
             </div>
-            <div>
-                <p>{name}</p>
+            <div className='flex flex-col mt-auto mb-auto'>
+                <p className='text-sm'>{name}</p>
                 <p><small>{symbol}</small></p>
             </div>
+           </div>
+           </Link>
         </div>
     );
 };
--- src/component/coins/Coins.js
+++ src/component/coins/Coins.js
@@ -9,8 +9,9 @@ const Coins = () => {
         .then(data => setCoins(data))
     },[])
     return (
-        <div>
-            <div>
+        <div className='px-4 pt-20 pb-24 mx-auto md:px-2'>
+            <h2>Available Crypto Currencies</h2>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-items-center'>
                 {
                     coins.map(coin => <Coin key={coin.id} coin={coin}></Coin>)
                 }
